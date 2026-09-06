d0be102f1b16c11dd9d23ea6946609a910ef8b45
vmdvdv-npt <vmdvdv@gmail.com>
2026-09-06 15:25:22 +0400

    Lock registration CLI default komi to 0.5

--- tests/test_gocube_default_komi.py
+++ tests/test_gocube_default_komi.py
@@ -2,6 +2,7 @@ import pytest
 
 from alphazero.envs.gocube.game import Cube4JapaneseGame, Torus9JapaneseGame, DEFAULT_KOMI
 from alphazero.envs.gocube.integration.manifest import RunManifest
+from alphazero.envs.gocube.integration.register_run import parse_args as parse_register_args
 
 
 def test_global_gocube_default_komi_is_half_point():
@@ -20,3 +21,12 @@ def test_new_manifest_defaults_to_half_point_komi():
         size=4,
     )
     assert manifest.komi == pytest.approx(0.5)
+
+
+def test_register_run_cli_defaults_to_half_point_komi():
+    args = parse_register_args([
+        "--run-name", "legacy-test",
+        "--topology", "cube",
+        "--size", "4",
+    ])
+    assert args.komi == pytest.approx(0.5)
