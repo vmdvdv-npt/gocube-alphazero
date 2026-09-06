7e47a5ead53bd0e5b4abde8a8b27e494c114b6be
vmdvdv-npt <vmdvdv@gmail.com>
2026-09-06 15:14:11 +0400

    Lock Cube and Torus default komi to 0.5

---
+++ tests/test_gocube_default_komi.py
@@ -0,0 +1,22 @@
+import pytest
+
+from alphazero.envs.gocube.game import Cube4JapaneseGame, Torus9JapaneseGame, DEFAULT_KOMI
+from alphazero.envs.gocube.integration.manifest import RunManifest
+
+
+def test_global_gocube_default_komi_is_half_point():
+    assert DEFAULT_KOMI == pytest.approx(0.5)
+
+
+def test_cube_and_torus_share_half_point_default_komi():
+    assert Cube4JapaneseGame.KOMI == pytest.approx(0.5)
+    assert Torus9JapaneseGame.KOMI == pytest.approx(0.5)
+
+
+def test_new_manifest_defaults_to_half_point_komi():
+    manifest = RunManifest.create(
+        run_name="komi05-default-test",
+        topology="cube",
+        size=4,
+    )
+    assert manifest.komi == pytest.approx(0.5)
