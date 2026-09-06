0b1cbb0e645b927050c0680dfb176dd4b318a98c
vmdvdv-npt <vmdvdv@gmail.com>
2026-09-06 14:00:04 +0400

    Wire four-head search outputs and phase telemetry through self-play

--- alphazero/SelfPlayAgent.pyx
+++ alphazero/SelfPlayAgent.pyx
@@ -9,13 +9,14 @@ import time
 
 from alphazero.MCTS import MCTS
 from alphazero.envs.gocube.records import reserve_game_id
+from alphazero.search_contract import GOCUBE_KATAGO_V3_SEARCH_UTILITY_MODE
 
 
 class SelfPlayAgent(mp.Process):
     def __init__(self, id, game_cls, ready_queue, batch_ready, batch_tensor, policy_tensor,
                  value_tensor, output_queue, result_queue, complete_count, games_played,
                  stop_event: mp.Event, pause_event: mp.Event(), args, _is_arena=False, _is_warmup=False,
-                 telemetry=None):
+                 telemetry=None, score_tensor=None, ownership_tensor=None):
         super().__init__()
         self.id = id
         self.game_cls = game_cls
@@ -28,6 +29,8 @@ class SelfPlayAgent(mp.Process):
             self.batch_size = self.batch_tensor.shape[0]
         self.policy_tensor = policy_tensor
         self.value_tensor = value_tensor
+        self.score_tensor = score_tensor
+        self.ownership_tensor = ownership_tensor
         self.output_queue = output_queue
         self.result_queue = result_queue
         self.games = []
@@ -43,6 +46,11 @@ class SelfPlayAgent(mp.Process):
         self._is_arena = _is_arena
         self._is_warmup = _is_warmup
         self.telemetry = telemetry
+        self.score_aware = (
+            getattr(args, 'search_utility_mode', 'legacy') == GOCUBE_KATAGO_V3_SEARCH_UTILITY_MODE
+        )
+        if self.score_aware and not _is_warmup and (score_tensor is None or ownership_tensor is None):
+            raise ValueError('score-aware SelfPlayAgent requires score_tensor and ownership_tensor')
         self.recording_enabled = bool(
             getattr(args, "gocube_recording_enabled", False) and not _is_arena
         )
@@ -60,13 +68,16 @@ class SelfPlayAgent(mp.Process):
             self._WARMUP_POLICY = torch.full((action_size,), 1 / action_size).to(policy_tensor.device)
             value_size = game_cls.num_players() + 1
             self._WARMUP_VALUE = torch.full((value_size,), 1 / value_size).to(policy_tensor.device)
+            if score_tensor is not None:
+                self._WARMUP_SCORE = torch.zeros((1,)).to(score_tensor.device)
+            if ownership_tensor is not None:
+                self._WARMUP_OWNERSHIP = torch.full(
+                    ownership_tensor.shape[1:], 1.0 / ownership_tensor.shape[-1]
+                ).to(ownership_tensor.device)
         self.fast = False
         for _ in range(self.batch_size):
             self.games.append(self.game_cls())
             self.histories.append([])
-            # IDs are reserved when a completed game is accepted into the
-            # iteration.  This avoids consuming an ID for a speculative batch
-            # slot that finishes after gamesPerIteration has already filled.
             self.game_ids.append(None)
             self.move_histories.append([])
             self.game_start_times.append(None)
@@ -144,6 +155,10 @@ class SelfPlayAgent(mp.Process):
             if self._is_warmup:
                 self.policy_tensor[i].copy_(self._WARMUP_POLICY)
                 self.value_tensor[i].copy_(self._WARMUP_VALUE)
+                if self.score_tensor is not None:
+                    self.score_tensor[i].copy_(self._WARMUP_SCORE)
+                if self.ownership_tensor is not None:
+                    self.ownership_tensor[i].copy_(self._WARMUP_OWNERSHIP)
                 continue
             data = torch.from_numpy(state.observation())
             if self._is_arena:
@@ -171,13 +186,113 @@ class SelfPlayAgent(mp.Process):
         for i in range(self.batch_size):
             self._check_pause()
             index = self.batch_indices[i] if self._is_arena else i
-            self._mcts(i).process_results(
-                self.games[i],
-                self.value_tensor[index].data.numpy(),
-                self.policy_tensor[index].data.numpy(),
-                False if self._is_arena else self.args.add_root_noise,
-                False if self._is_arena else self.args.add_root_temp
-            )
+            if self.score_aware:
+                self._mcts(i).process_search_results(
+                    self.games[i],
+                    self.value_tensor[index].data.numpy(),
+                    self.policy_tensor[index].data.numpy(),
+                    self.score_tensor[index].data.numpy(),
+                    self.ownership_tensor[index].data.numpy(),
+                    False if self._is_arena else self.args.add_root_noise,
+                    False if self._is_arena else self.args.add_root_temp,
+                )
+            else:
+                self._mcts(i).process_results(
+                    self.games[i],
+                    self.value_tensor[index].data.numpy(),
+                    self.policy_tensor[index].data.numpy(),
+                    False if self._is_arena else self.args.add_root_noise,
+                    False if self._is_arena else self.args.add_root_temp,
+                )
+
+    def _phase_bucket(self, game):
+        state = getattr(game, 'semantic_state', None)
+        if state is None:
+            return 'main'
+        phase = getattr(state, 'phase', 'main')
+        if phase == 'main' and int(getattr(state, 'consecutive_passes', 0)) == 1:
+            return 'main_after_one_pass'
+        if phase == 'cleanup1':
+            return 'cleanup1'
+        if phase == 'cleanup2':
+            return 'cleanup2'
+        return 'main'
+
+    def _phase_weight(self, bucket):
+        if bucket == 'main_after_one_pass':
+            return int(getattr(self.args, 'gocube_main_after_pass_weight', 1))
+        if bucket == 'cleanup1':
+            return int(getattr(self.args, 'gocube_cleanup1_weight', 1))
+        if bucket == 'cleanup2':
+            return int(getattr(self.args, 'gocube_cleanup2_weight', 1))
+        return 1
+
+    def _record_decision_telemetry(self, game, action):
+        state = getattr(game, 'semantic_state', None)
+        if state is None:
+            return
+        phase = getattr(state, 'phase', None)
+        player = int(game.player)
+        is_pass = int(action) == int(self.game_cls.pass_action())
+        if phase == 'main':
+            phase_key = 'main'
+        elif phase == 'cleanup1':
+            phase_key = 'cleanup1'
+        elif phase == 'cleanup2':
+            phase_key = 'cleanup2'
+        else:
+            return
+        self._telemetry_add(f'phase_{phase_key}_decisions')
+        self._telemetry_add(f'phase_{phase_key}_passes' if is_pass else f'phase_{phase_key}_placements')
+        if is_pass:
+            color = 'black' if player == 0 else 'white'
+            self._telemetry_add(f'pass_{phase_key}_{color}')
+        if phase == 'main' and is_pass and int(getattr(state, 'consecutive_passes', 0)) == 1:
+            turn = int(game.turns) + 1
+            self._telemetry_add('main_second_pass_count')
+            self._telemetry_add('main_second_pass_turn_sum', turn)
+            if turn <= 2:
+                self._telemetry_add('main_double_pass_within_2')
+            if turn <= 4:
+                self._telemetry_add('main_double_pass_within_4')
+            if turn <= 8:
+                self._telemetry_add('main_double_pass_within_8')
+
+    def _record_search_audit(self, game, action):
+        if not self.score_aware:
+            return
+        state = getattr(game, 'semantic_state', None)
+        if state is None or getattr(state, 'phase', None) != 'main' or int(getattr(state, 'consecutive_passes', 0)) != 1:
+            return
+        sample_probability = float(getattr(self.args, 'gocube_search_audit_probability', 0.25))
+        if sample_probability < 1.0 and np.random.random_sample() >= sample_probability:
+            return
+        diagnostic = self._mcts_for_diagnostic(game)
+        if not diagnostic:
+            return
+        self._telemetry_add('search_audited_positions')
+        self._telemetry_add('search_pass_root_prior_sum', diagnostic['pass_root_prior'])
+        self._telemetry_add('search_pass_visit_fraction_sum', diagnostic['pass_visit_fraction'])
+        self._telemetry_add('search_pass_win_utility_sum', diagnostic['pass_win_utility'])
+        self._telemetry_add('search_pass_score_utility_sum', diagnostic['pass_score_utility'])
+        self._telemetry_add('search_pass_combined_utility_sum', diagnostic['pass_combined_utility'])
+        self._telemetry_add('search_best_nonpass_score_gain_sum', diagnostic['best_nonpass_score_gain'])
+        self._telemetry_add('search_best_nonpass_win_delta_sum', diagnostic['best_nonpass_win_delta'])
+        pass_action = int(self.game_cls.pass_action())
+        pass_best = int(self._mcts_current.best_action(game)) == pass_action
+        if pass_best and diagnostic['score_dominated_pass']:
+            self._telemetry_add('search_score_dominated_pass')
+
+    @property
+    def _mcts_current(self):
+        # Set transiently by playMoves before calling _record_search_audit.
+        return self.__mcts_current
+
+    def _mcts_for_diagnostic(self, game):
+        try:
+            return self._mcts_current.pass_diagnostic(game)
+        except Exception:
+            return {}
 
     def playMoves(self):
         recording_enabled = getattr(self, "recording_enabled", False)
@@ -186,12 +301,16 @@ class SelfPlayAgent(mp.Process):
             self.temps[i] = self.args.temp_scaling_fn(
                 self.temps[i], self.games[i].turns, self.game_cls.max_turns()
             ) if not self._is_arena else self.args.arenaTemp
-            policy = self._mcts(i).probs(self.games[i], self.temps[i])
+            current_mcts = self._mcts(i)
+            self.__mcts_current = current_mcts
+            policy = current_mcts.probs(self.games[i], self.temps[i])
             action = np.random.choice(self.games[i].action_size(), p=policy)
             if not self._is_arena:
+                self._record_decision_telemetry(self.games[i], action)
+                self._record_search_audit(self.games[i], action)
                 self._telemetry_add('fast_decisions' if self.fast else 'regular_decisions')
             if not self.fast and not self._is_arena:
-                self.histories[i].append((self.games[i].clone(), self._mcts(i).probs(self.games[i])))
+                self.histories[i].append((self.games[i].clone(), current_mcts.probs(self.games[i])))
             if recording_enabled:
                 state = getattr(self.games[i], "semantic_state", None)
                 self.game_start_times[i] = self.game_start_times[i] or time.time()
@@ -205,7 +324,7 @@ class SelfPlayAgent(mp.Process):
             if self._is_arena:
                 [mcts.update_root(self.games[i], action) for mcts in self.mcts[i]]
             else:
-                self._mcts(i).update_root(self.games[i], action)
+                current_mcts.update_root(self.games[i], action)
             self.games[i].play_action(action)
             if self.args.mctsResetThreshold and self.games[i].turns >= self.next_reset[i]:
                 self.mcts[i] = self._get_mcts()
@@ -262,19 +381,18 @@ class SelfPlayAgent(mp.Process):
                     for hist in self.histories[i]:
                         self._check_pause()
                         self._telemetry_add('base_positions')
-                        is_endgame = bool(
-                            hasattr(hist[0], "is_endgame_training_state")
-                            and hist[0].is_endgame_training_state()
-                        )
+                        bucket = self._phase_bucket(hist[0])
+                        is_endgame = bucket != 'main'
                         if is_endgame:
                             self._telemetry_add('base_endgame_positions')
                         data = hist[0].symmetries(hist[1]) if self.args.symmetricSamples else ((hist[0], hist[1]),)
-                        repeat = 1
-                        endgame_weight = int(getattr(self.args, "gocube_endgame_sample_weight", 1))
-                        if endgame_weight > 1 and is_endgame:
-                            repeat = endgame_weight
+                        repeat = self._phase_weight(bucket)
+                        if repeat < 1:
+                            raise ValueError(f'phase sample weight must be at least 1, got {repeat}')
                         for state, pi in data:
                             self._check_pause()
+                            self._telemetry_add(f'samples_{bucket}')
+                            self._telemetry_add(f'weighted_samples_{bucket}', repeat)
                             sample = (state.observation(), pi, np.array(winstate, dtype=np.float32))
                             if auxiliary:
                                 sample = sample + (score_target, ownership_target)
